e13943135dcaaa9fee3177e5994ec60dc17310c7
Mike Abner <mike.abner@gmail.com>
2010-01-24 15:34:17 -0800

    added seeds for user and testusers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+User.create(:email => "mike.abner@gmail.com", :password => "password", :admin => true, :nickname => "Mike")
+User.create(:email => "mike.test@tlp.com", :password => "password", :admin => false, :nickname => "Test Mike")
+User.create(:email => "ekroboth@gmail.com", :password => "password", :admin => true, :nickname => "Liz")
+User.create(:email => "testliz@tlp.com", :password => "password", :admin => false, :nickname => "Test Liz")
+User.create(:email => "pevzner@gmail.com", :password => "password", :admin => true, :nickname => "Lev")
+User.create(:email => "testlev@tlp.com", :password => "password", :admin => false, :nickname => "Test Lev")
+User.create(:email => "kdred39@gmail.com", :password => "password", :admin => true, :nickname => "Kate")
+User.create(:email => "tetkate@tlp.com", :password => "password", :admin => false, :nickname => "Test Kate")
