4ef377412b4620e74fa57009198c231956678c10
Mike Abner <mike.abner@gmail.com>
2010-01-29 15:56:28 -0800

    added a new/edit form for games

--- app/controllers/admin_games_controller.rb
+++ app/controllers/admin_games_controller.rb
@@ -6,4 +6,41 @@ class AdminGamesController < ApplicationController
     end
     @games = Game.all
   end
+  def show
+    @game = Game.find(params[:id])
+  end
+  def new
+    @game = Game.new
+    render :action => 'edit'
+  end
+  def edit
+    @game = Game.find(params[:id])
+  end
+  def create
+    @game = Game.new(params[:game])
+    
+    if @game.save
+      flash[:success] = "The game has been saved successfully!"
+      render :action => 'show'
+    else
+      flash[:error] = "The game could not be saved."
+      render :action => 'edit'
+    end
+  end
+  def update
+    @game = Game.find(params[:id])
+    
+    if @game.update_attributes(params[:game])
+      flash[:success] = "The game has been updated successfully!"
+      render :action => 'show'
+    else
+      flash[:error] = "The game could not be updated."
+      render :action => 'edit'
+    end
+  end
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to "/admin_games"
+  end
 end
