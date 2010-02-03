85e6964dec7e0770139f858a1f04fe8c9a623a4b
Mike Abner <mike.abner@gmail.com>
2010-02-02 18:06:31 -0800

    more tests

--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class GameTest < ActiveSupport::TestCase
   test "should create constants when loaded" do
     g = Game.find(1)
-    assert Game::MEGAMILLIONS_CA
+    assert Game::MEGAMILLIONS_MULTI
   end
   
   test "should configure generator when loaded" do
@@ -31,9 +31,61 @@ class GameTest < ActiveSupport::TestCase
     assert g.super_duplicates == false
   end
   
+  test "has states" do
+    g = Game.find(1)
+    assert g.states
+    assert g.states.size == 2
+    
+    g = Game.find(2)
+    assert g.states
+    assert g.states.size == 1
+  end
+  
   test "should allow hyphen in name: issue 21" do
     g = Game.new
     g.name = "Twenty-Five"
     assert g.save!
+  end
+  
+  test "try to test leading zeros in 1 digit random numbers: issue 22" do
+    g = Game.find(3)
+    (0..1000).each do |i|
+      n = g.generator.generate_random_numbers
+      n = n[:numbers]
+      if n[0].to_i >= 100 and n[0].to_i < 1000
+        assert n[0].to_s.starts_with? "0"
+      end
+      if n[0].to_i > 10 and n[0].to_i < 100
+        assert n[0].to_s.starts_with? "00"
+      end
+      if n[0].to_i < 10
+        assert n[0].to_s.starts_with?"000"
+      end
+    end
+  end
+  
+  test "when sequence is false balls are ordered" do
+    g = Game.find(1)
+    n = g.generator.generate_random_numbers
+    r = n[:numbers]
+    s = n[:super_numbers]
+    
+    i = 0
+    r.each do |x|
+      assert i < x
+      i = x
+    end
+    i = 0
+    s.each do |x|
+      assert i < x
+      i = x
+    end
+  end
+  test "when sequence is true balls aren't ordered" do
+    #how?
+  end
+  
+  test "when regular_duplicates is false there cannot be duplicates" do
+    #hmmm
   end
 end
--- test/unit/states_test.rb
+++ test/unit/states_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class StatesTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "creates a constant when loaded" do
+    s = State.find(1)
+    assert State::CALIFORNIA
+    assert State::CA
+  end
+  
+  test "has games" do
+    s = State.find(1)
+    assert s.games
+    assert s.games.size == 3
+    
+    s = State.find(2)
+    assert s.games
+    assert s.games.size == 1
   end
 end
