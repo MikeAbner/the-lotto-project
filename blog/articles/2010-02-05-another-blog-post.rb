bd6932bdba118289421503265a849ba2e0df316a
Mike Abner <mike.abner@gmail.com>
2010-02-05 09:59:44 -0800

    a new blog post

---
+++ blog/articles/2010-02-05-another-blog-post.rb
@@ -0,0 +1,13 @@
+title: Another Blog Post
+author: Mike
+date: 2010/02/05
+
+This is yet another blog post!  Just trying to see how this works.  :o)
+
+Dorothy lived in the midst of the great Kansas prairies, with Uncle Henry, 
+who was a farmer, and Aunt Em, who was the farmer's wife.
+
+This is a test.
+
+Changing something so maybe heroku picks up the change.
+
